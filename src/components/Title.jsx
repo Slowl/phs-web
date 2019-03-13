fbece7ffe844ef8d1716bb5acc7fc332d4f4cc6c
HediK <hedi0475@gmail.com>
2019-03-13 14:06:15 +0100

    added title of the website

---
+++ src/components/Title.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const TitleContainer = styled.div`
+  @import url('https://fonts.googleapis.com/css?family=Rubik:400,700');
+  font-family: 'Rubik', sans-serif;
+  display: block;
+  color: white;
+  font-size: 92px;
+  font-weight: 400;
+  text-align: center;
+  width: 30%;
+  padding: .2em;
+  margin: 1em auto 0;
+
+  span {
+    background-color: #ff9900;
+    color: black;
+    padding: .1em;
+    border-radius: 10px;
+  }
+
+  @media screen and (max-width: 45em) {
+      font-size: 72px;
+      width: 100%;
+  }
+
+`
+
+const Title = () => {
+
+  return (
+    <TitleContainer> WH<span>AT ?</span> </TitleContainer>
+  )
+}
+
+export default Title
