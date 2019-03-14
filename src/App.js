f13b4f55a4b23df945bfa557a60c064ff72a394d
HediK <hedi0475@gmail.com>
2019-03-14 09:14:40 +0100

    deplaced font import

--- src/App.js
+++ src/App.js
@@ -6,20 +6,25 @@ import MainApp from './pages/MainApp'
 import About from './pages/About'
 import Footer from './components/Footer'
 
-const Container = styled(Router)`
+const Container = styled.div`
+  @import url('https://fonts.googleapis.com/css?family=Rubik:400,700');
+  @import url('https://fonts.googleapis.com/css?family=Montserrat:200,300,400');
+`
+
+const CustomRouter = styled(Router)`
   height: calc(100vh - 6em);
 `
 
 const App = () => {
     return (
-      <div>
+      <Container>
         <Nav />
-        <Container>
-          <MainApp path="home" />
+        <CustomRouter>
+          <MainApp path="/" />
           <About path="about" />
-        </Container>
+        </CustomRouter>
         <Footer />
-      </div>
+      </Container>
     );
   }
 
